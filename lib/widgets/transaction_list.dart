91ba65021dd4ae80737445bab94a9daed20c4e57
A-Stanley <astanley@uplandsoftware.com>
2020-10-08 16:22:24 +0100

    added midea query for responsiveness

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -11,67 +11,64 @@ class TransactionList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 400,
-      child: transactions.isEmpty
-          ? Column(
-              children: <Widget>[
-                Text(
-                  'No transactions added yet!',
+    return transactions.isEmpty
+      ? Column(
+          children: <Widget>[
+            Text(
+              'No transactions added yet!',
+              style: Theme.of(context).textTheme.headline6,
+            ),
+            SizedBox(
+              height: 40,
+            ),
+            Container(
+              height: 200,
+              child: Image.asset(
+                'assets/images/waiting.png',
+                fit: BoxFit.cover,
+              ),
+            ),
+          ],
+        )
+      : ListView.builder(
+          itemBuilder: (ctx, index) {
+            return Card(
+              elevation: 5,
+              margin: EdgeInsets.symmetric(
+                vertical: 8,
+                horizontal: 5,
+              ),
+              child: ListTile(
+                leading: CircleAvatar(
+                  radius: 30,
+                  child: Padding(
+                    padding: EdgeInsets.all(10),
+                    child: FittedBox(
+                      child: Text('\$${transactions[index].amount}'),
+                    ),
+                  ),
+                ),
+                title: Text(
+                  transactions[index].title,
                   style: Theme.of(context).textTheme.headline6,
                 ),
-                SizedBox(
-                  height: 40,
-                ),
-                Container(
-                  height: 200,
-                  child: Image.asset(
-                    'assets/images/waiting.png',
-                    fit: BoxFit.cover,
+                subtitle: Text(
+                  DateFormat.yMMMd().format(transactions[index].date),
+                  style: TextStyle(
+                    fontSize: 10,
+                    color: Colors.grey,
                   ),
                 ),
-              ],
-            )
-          : ListView.builder(
-              itemBuilder: (ctx, index) {
-                return Card(
-                  elevation: 5,
-                  margin: EdgeInsets.symmetric(
-                    vertical: 8,
-                    horizontal: 5,
-                  ),
-                  child: ListTile(
-                    leading: CircleAvatar(
-                      radius: 30,
-                      child: Padding(
-                        padding: EdgeInsets.all(10),
-                        child: FittedBox(
-                          child: Text('\$${transactions[index].amount}'),
-                        ),
-                      ),
-                    ),
-                    title: Text(
-                      transactions[index].title,
-                      style: Theme.of(context).textTheme.headline6,
-                    ),
-                    subtitle: Text(
-                      DateFormat.yMMMd().format(transactions[index].date),
-                      style: TextStyle(
-                        fontSize: 10,
-                        color: Colors.grey,
-                      ),
-                    ),
-                    trailing: IconButton(
-                      icon: Icon(Icons.delete),
-                      color: Theme.of(context).errorColor,
-                      onPressed: () => deleteTx(transactions[index].id),
-                    ),
-                  ),
-                );
-              },
-              itemCount: transactions.length,
-            ),
-    );
+                trailing: IconButton(
+                  icon: Icon(Icons.delete),
+                  color: Theme.of(context).errorColor,
+                  onPressed: () => deleteTx(transactions[index].id),
+                ),
+              ),
+            );
+          },
+          itemCount: transactions.length,
+        );
   }
 }
 
