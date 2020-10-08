d45deeb30ff9dd249f584dfaaa1e3c1ec360a6b7
A-Stanley <astanley@uplandsoftware.com>
2020-10-08 18:32:52 +0100

    refactor code for cupertino widget

--- lib/widgets/new_transaction.dart
+++ lib/widgets/new_transaction.dart
@@ -1,4 +1,7 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:intl/intl.dart';
 
 class NewTransaction extends StatefulWidget {
@@ -89,7 +92,13 @@ class _NewTransactionState extends State<NewTransaction> {
                             : 'Picked Date: ${DateFormat.yMd().format(_selectedDate)}',
                       ),
                     ),
-                    FlatButton(
+                    Platform.isIOS ? CupertinoButton(
+                      child: Text(
+                        'Choose Date',
+                        style: TextStyle(fontWeight: FontWeight.bold),
+                      ), 
+                      onPressed: _presentDatePicker,) 
+                      : FlatButton(
                       textColor: Theme.of(context).primaryColor,
                       onPressed: _presentDatePicker,
                       child: Text(
