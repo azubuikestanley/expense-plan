f1d8d9dd36133513b29eabbbbcdec171c924f79c
A-Stanley <astanley@uplandsoftware.com>
2020-10-08 23:37:12 +0100

    refactor a widget by extracting

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -14,33 +14,34 @@ class TransactionList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return transactions.isEmpty
-        ? LayoutBuilder(builder: (ctx, constraints) {
-            return Column(
-              children: <Widget>[
-                Text(
-                  'No transactions added yet!',
-                  style: Theme.of(context).textTheme.headline6,
+      ? LayoutBuilder(builder: (ctx, constraints) {
+          return Column(
+            children: <Widget>[
+              Text(
+                'No transactions added yet!',
+                style: Theme.of(context).textTheme.headline6,
+              ),
+              SizedBox(
+                height: 40,
+              ),
+              Container(
+                height: constraints.maxHeight * 0.6,
+                child: Image.asset(
+                  'assets/images/waiting.png',
+                  fit: BoxFit.cover,
                 ),
-                SizedBox(
-                  height: 40,
-                ),
-                Container(
-                  height: constraints.maxHeight * 0.6,
-                  child: Image.asset(
-                    'assets/images/waiting.png',
-                    fit: BoxFit.cover,
-                  ),
-                ),
-              ],
-            );
-          })
-        : ListView.builder(
-            itemBuilder: (ctx, index) {
-              return TransactionItem(transaction: transactions[index], deleteTx: deleteTx,);
-            },
-            itemCount: transactions.length,
+              ),
+            ],
           );
+        })
+      : ListView.builder(
+          itemBuilder: (ctx, index) {
+            return TransactionItem(
+              transaction: transactions[index],
+              deleteTx: deleteTx,
+            );
+          },
+          itemCount: transactions.length,
+        );
   }
 }
-
-
