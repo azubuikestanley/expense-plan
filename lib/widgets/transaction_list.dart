e11436636cc432d2bb1492174379c89f24066ed2
A-Stanley <astanley@uplandsoftware.com>
2020-10-14 10:01:14 +0100

    added key to listView

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -14,34 +14,36 @@ class TransactionList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return transactions.isEmpty
-      ? LayoutBuilder(builder: (ctx, constraints) {
-          return Column(
-            children: <Widget>[
-              Text(
-                'No transactions added yet!',
-                style: Theme.of(context).textTheme.headline6,
-              ),
-              SizedBox(
-                height: 40,
-              ),
-              Container(
-                height: constraints.maxHeight * 0.6,
-                child: Image.asset(
-                  'assets/images/waiting.png',
-                  fit: BoxFit.cover,
+        ? LayoutBuilder(builder: (ctx, constraints) {
+            return Column(
+              children: <Widget>[
+                Text(
+                  'No transactions added yet!',
+                  style: Theme.of(context).textTheme.headline6,
                 ),
-              ),
-            ],
-          );
-        })
-      : ListView.builder(
-          itemBuilder: (ctx, index) {
-            return TransactionItem(
-              transaction: transactions[index],
-              deleteTx: deleteTx,
+                SizedBox(
+                  height: 40,
+                ),
+                Container(
+                  height: constraints.maxHeight * 0.6,
+                  child: Image.asset(
+                    'assets/images/waiting.png',
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ],
             );
-          },
-          itemCount: transactions.length,
-        );
+          })
+        : ListView(
+            children: transactions
+                .map(
+                  (tx) => TransactionItem(
+                    key: ValueKey(tx.id),
+                    transaction: tx,
+                    deleteTx: deleteTx,
+                  ),
+                )
+                .toList(),
+          );
   }
 }
